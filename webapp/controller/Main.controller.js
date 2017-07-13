dea615331d799d231d9b7261ae0ffc2bcc24b59c
NunoRodrigues15 <rodriguesnunorodrigues15@gmail.com>
2017-07-13 15:40:23 +0000

    working main change spans in grid

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,33 +1,32 @@
-sap.ui.define(["sap/ui/core/mvc/Controller",
-    "sap/m/MessageBox",
-    "./utilities",
-    "sap/ui/core/routing/History"
-    ], function(BaseController) {
-    "use strict";
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/UIComponent",
+	"sap/m/MessageBox",
+	"./utilities"
+], function(BaseController, MessageBox, Utilities, History) {
+	"use strict";
 
-    return BaseController.extend("odkasfactory.reservasalas.controller.Main", {
-    handleRouteMatched: function (oEvent) {
-            		
-		var oParams = {}; 
-		
-		if (oEvent.mParameters.data.context) { 
-		    this.sContext = oEvent.mParameters.data.context;
-		    var oPath; 
-		    if (this.sContext) { 
-		        oPath = {path: "/" + this.sContext, parameters: oParams}; 
-		        this.getView().bindObject(oPath);
-		    } 
+	return BaseController.extend("odkasfactory.reservasalas.controller.Main", {
+		//  handleRouteMatched: function (oEvent) {
+
+		// var oParams = {}; 
+
+		// if (oEvent.mParameters.data.context) { 
+		//     this.sContext = oEvent.mParameters.data.context;
+		//     var oPath; 
+		//     if (this.sContext) { 
+		//         oPath = {path: "/" + this.sContext, parameters: oParams}; 
+		//         this.getView().bindObject(oPath);
+		//     } 
+		// }
+
+		//      },
+		onInit: function() {
+
+			// this.mBindingOptions = {};
+			// this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			// this.oRouter.getTarget("Main").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+
 		}
-		
-		
-        },
-onInit: function () {
-            		
-        this.mBindingOptions = {};
-        this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-        this.oRouter.getTarget("Main").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
-
-
-        }
+	});
 });
-}, /* bExport= */true);
