b2ec129c4d6040c94ca525bd13f9f2c9670e759f
AndreFerreira18 <andrefferreira@outlook.pt>
2017-07-13 10:25:44 +0000

    Commented unused code. Working on Routing between Initial Modal and Help Section.

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,18 +2,18 @@ sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/Device",
 	"odkasfactory/reservasalas/model/models",
-    	"odkasfactory/reservasalas/localService/mockserver"
-    ], function(UIComponent, Device, models, server) {
-    	"use strict";
-        
-    	// TODO remove the following demo code
-    	// ---------------------------- TEMP MOCKSERVER CODE------------------------------------------
-    	server.init();
-    	// ---------------------------- END TEMP MOCKSERVER CODE--------------------------------------
-    	
-	var navigationWithContext = {
-	
-	};
+	"odkasfactory/reservasalas/localService/mockserver"
+], function(UIComponent, Device, models, server) {
+	"use strict";
+
+	// TODO remove the following demo code
+	// ---------------------------- TEMP MOCKSERVER CODE------------------------------------------
+	// server.init();
+	// ---------------------------- END TEMP MOCKSERVER CODE--------------------------------------
+
+	// var navigationWithContext = {
+
+	// };
 
 	return UIComponent.extend("odkasfactory.reservasalas.Component", {
 
@@ -27,39 +27,40 @@ sap.ui.define([
 		 * @override
 		 */
 		init: function() {
-			// set the device model
-			this.setModel(models.createDeviceModel(), "device");
-			// set the FLP model
-			this.setModel(models.createFLPModel(), "FLP");
-			
+			// // set the device model
+			// this.setModel(models.createDeviceModel(), "device");
+			// // set the FLP model
+			// this.setModel(models.createFLPModel(), "FLP");
+
 			// set the dataSource model
-			this.setModel(new sap.ui.model.json.JSONModel({"uri":"\"/here/goes/your/serviceUrl/\""}), "dataSource");
-			
+			// this.setModel(new sap.ui.model.json.JSONModel({
+			// 	"uri": "\"/here/goes/your/serviceUrl/\""
+			// }), "dataSource");
 
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
 
 			// create the views based on the url/hash
 			this.getRouter().initialize();
-		},
-
-		createContent: function() {
-			var app = new sap.m.App({
-				id: "App"
-			});
-			var appType = "App";
-			var appBackgroundColor = "#FFFFFF";
-			if (appType === "App" && appBackgroundColor) {
-				app.setBackgroundColor(appBackgroundColor);
-			}
-
-			return app;
-		},
-
-		getNavigationPropertyForNavigationWithContext: function(sEntityNameSet, targetPageName) {
-			var entityNavigations = navigationWithContext[sEntityNameSet];
-			return entityNavigations == null ? null : entityNavigations[targetPageName];
 		}
+
+		// createContent: function() {
+		// 	var app = new sap.m.App({
+		// 		id: "App"
+		// 	});
+		// 	var appType = "App";
+		// 	var appBackgroundColor = "#FFFFFF";
+		// 	if (appType === "App" && appBackgroundColor) {
+		// 		app.setBackgroundColor(appBackgroundColor);
+		// 	}
+
+		// 	return app;
+		// },
+
+		// getNavigationPropertyForNavigationWithContext: function(sEntityNameSet, targetPageName) {
+		// 	var entityNavigations = navigationWithContext[sEntityNameSet];
+		// 	return entityNavigations == null ? null : entityNavigations[targetPageName];
+		// }
 	});
 
-});
+}, /*bExport= */ true);
