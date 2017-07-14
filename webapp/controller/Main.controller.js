37d49d411e02463941fca95bb5660395b837d030
NunoRodrigues15 <rodriguesnunorodrigues15@gmail.com>
2017-07-14 14:36:15 +0000

    working main grid

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -7,6 +7,20 @@ sap.ui.define([
 	"use strict";
 
 	return BaseController.extend("odkasfactory.reservasalas.controller.Main", {
+		// onAfterRendering: function() {
+		// 	var containerSideBar =  sap.ui.getCore().byId("__panel0");
+		// 	containerSideBar.oParent.aCustomStyleClasses=["sapUiRespGridSpanL3", "sapUiRespGridSpanM5", "sapUiRespGridSpanS12", "sapUiRespGridSpanXL3"];
+			
+		// 	var containerFloorSelector =  sap.ui.getCore().byId("__grid1");
+		// 	containerFloorSelector.oParent.aCustomStyleClasses=["sapUiRespGridSpanL3", "sapUiRespGridSpanM7", "sapUiRespGridSpanS12", "sapUiRespGridSpanXL3"];
+			
+		// 	var containerMatrix =  sap.ui.getCore().byId("__bar1");
+		// 	containerMatrix.oParent.aCustomStyleClasses=["sapUiRespGridSpanL6", "sapUiRespGridSpanM7", "sapUiRespGridSpanS12", "sapUiRespGridSpanXL6"];
+		
+		// 	var containerFooter =  sap.ui.getCore().byId("__table0");
+		// 	containerFooter.oParent.aCustomStyleClasses=["sapUiRespGridSpanL9", "sapUiRespGridSpanM7", "sapUiRespGridSpanS12", "sapUiRespGridSpanXL9"];
+
+		// },
 		//  handleRouteMatched: function (oEvent) {
 
 		// var oParams = {}; 
