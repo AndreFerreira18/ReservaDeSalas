3ec1c05e2f01a0dcbb0472e4f24f9237738406f1
NunoRodrigues15 <rodriguesnunorodrigues15@gmail.com>
2017-07-21 14:20:30 +0000

    Added Apoint Select Event Date Manipulation

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -58,16 +58,18 @@ sap.ui.define([
 			this.getView().byId("sidebarForm").addStyleClass("sidebarForm");
 			this.getView().byId("sb_meeting_type").addStyleClass("meeting");
 		},
-		// handleAppointmentSelect: function (oEvent) {
-		// 	var oAppointment = oEvent.getParameter("appointment");
-		// 	if (oAppointment) {
-		// 		alert("Appointment selected: " + oAppointment.getTitle());
-		// 	}else {
-		// 		var aAppointments = oEvent.getParameter("appointments");
-		// 		var sValue = aAppointments.length + " Appointments selected";
-		// 		alert(sValue);
-		// 	}
-		// },
+
+		handleAppointmentSelect: function(oEvent) {
+			var oAppointment = oEvent.getParameter("appointment");
+			if (oAppointment) {
+				alert("Appointment selected: " + oAppointment.getTitle() + "Start Time: " + oAppointment.mProperties.startDate + "End Time: " +
+					oAppointment.mProperties.endDate);
+			} else {
+				var aAppointments = oEvent.getParameter("appointments");
+				var sValue = aAppointments.length + " Appointments selected";
+				alert(sValue);
+			}
+		},
 
 		handleIntervalSelect: function(event) {
 			var oPC = event.oSource;
@@ -78,28 +80,80 @@ sap.ui.define([
 			var modelPC = this.getView().byId("PC1").getModel();
 			var data = modelPC.getData();
 			var index = -1;
-			var newAppointment = {start: startDate,
-					            end: endDate,
-					            title: "new appointment",
-					            type: "Type09"};
+
 			if (row) {
 				index = oPC.indexOfRow(row);
-				data.rooms[index].appointments.push(newAppointment);
+				var isNewAppointment = true;
+				var appointments = data.rooms[index].appointments;
+				for (var i = 0; i < appointments.length; i++) {
+					if (this._inRange(startDate, appointments[i].start, appointments[i].end) || this._isContiguousDates(appointments[i].end,
+							startDate)) {
+						data.rooms[index].appointments[i].end = endDate;
+						isNewAppointment = false;
+					}
+					if (this._inRange(endDate, appointments[i].start, appointments[i].end) || this._isContiguousDates(endDate, appointments[i].start)) {
+						data.rooms[index].appointments[i].start = startDate;
+						isNewAppointment = false;
+					}
+
+				}
+				if (isNewAppointment) {
+					var newAppointment = {
+						start: startDate,
+						end: endDate,
+						title: "new appointment",
+						type: "Type09"
+					};
+					data.rooms[index].appointments.push(newAppointment);
+				}
 			} else {
 				var selectedRows = oPC.getSelectedRows();
-				for (var i = 0; i < selectedRows.length; i++) {
+				for (i = 0; i < selectedRows.length; i++) {
 					index = oPC.indexOfRow(selectedRows[i]);
 					data.rooms[index].appointments.push(newAppointment);
 				}
 			}
 			modelPC.setData(data);
 		},
-		
-		_getArrayDate: function(date){
+
+		_isSameInfoReservation: function(appointments, appointment) {
+			for (var i = 0; i < appointments.length; i++) {
+				for (var tempAppointments in appointments[i]) {
+					for (var tempAppointment in appointment)
+						if (tempAppointments === tempAppointment && (tempAppointment !== "start" || tempAppointment !== "end")) {
+							return true;
+						}
+				}
+			}
+			return false;
+		},
+
+		_inRange: function(date, startDate, endDate) {
+			var d = this.dateFormatter(date);
+			var sd = this.dateFormatter(startDate);
+			var ed = this.dateFormatter(endDate);
+			if (d > sd && d < ed) {
+				return true;
+			}
+			return false;
+		},
+
+		_isContiguousDates: function(endDate, startDate) {
+			var sd = this.dateFormatter(startDate);
+			var ed = this.dateFormatter(endDate);
+			// Adds 1 minute to the end date
+			ed.setTime(ed.getTime() + (60 * 1000));
+			if (ed.valueOf() === sd.valueOf()) {
+				return true;
+			}
+			return false;
+		},
+
+		_getArrayDate: function(date) {
 			var dateMonth = date.getMonth().toString();
 			var strDate = date.toString().split(" ");
 			var strTime = strDate[4].split(":");
-			return [strDate[3],dateMonth,strDate[2],strTime[0],strTime[1]];
+			return [strDate[3], dateMonth, strDate[2], strTime[0], strTime[1]];
 		},
 
 		_refreshShownFloors: function(vBox) { // not working
@@ -116,13 +170,13 @@ sap.ui.define([
 				return vBox;
 			}
 		},
-				
+
 		onDataReceived: function(channel, event, data) {
 			this.filters = data;
 		},
-		
-		onChangeData: function(oEvent){
-			
+
+		onChangeData: function(oEvent) {
+
 		}
 	});
 });
